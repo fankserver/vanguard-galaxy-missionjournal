d68e494fffa03060c71cae1dbb768db0e708d89e
Florian Kinder <florian.kinder@fankserver.com>
2026-05-29 16:53:13 +0200

    chore(release): bump PluginVersion to 0.1.3

--- VGMissionJournal/Plugin.cs
+++ VGMissionJournal/Plugin.cs
@@ -22,7 +22,7 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PluginGuid    = "vgmissionjournal";
     public const string PluginName    = "Vanguard Galaxy Mission Journal";
-    public const string PluginVersion = "0.1.2";
+    public const string PluginVersion = "0.1.3";
 
     internal static Plugin          Instance { get; private set; } = null!;
     internal static ManualLogSource Log      { get; private set; } = null!;
